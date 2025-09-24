d6d683d5c626859488491b5d1af5cbed75f296fd
Arunjangir8 <arunjangir9987@example.com>
2025-09-24 15:46:59 +0530

    Hosting in netlify

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://food-five-swart.vercel.app'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://newfoodie.netlify.app'],
   credentials: true
 }));
 
